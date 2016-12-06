78234ca198a5fde31e6fe08fb992a68409a0bcb6
Arx Cruz <arxcruz@redhat.com>
2016-12-06 14:23:51 +0100

    Adding test_utils unit tests

--- tempestmail/tests/test_utils.py
+++ tempestmail/tests/test_utils.py
@@ -1,14 +1,46 @@
+import datetime
+import os
 import unittest
 
 from httmock import all_requests, HTTMock
+from six.moves.urllib.parse import urljoin
+
+from tempestmail import tests
 
 import tempestmail.utils as utils
+
+
+def _get_console_content(filename='console.html'):
+    console_log = os.path.join(os.path.dirname(tests.__file__),
+                               'fixtures', 'console',
+                               filename)
+    with open(console_log, 'r') as console_file:
+        return console_file.read()
 
 
 @all_requests
 def get_html_request_ok(url, request):
     return {'status_code': 200,
             'content': b'Feeling lucky?'}
+
+
+@all_requests
+def get_console_log(url, request):
+    if url.path == '/console.html.gz' and url.netloc == 'www.found.com':
+        return {'status_code': 200,
+                'content': _get_console_content()}
+
+    if url.path == '/console.html.gz' and url.netloc == 'www.google.com':
+        return {'status_code': 403,
+                'content': None}
+
+    if url.path == '/console.html.gz' and url.netloc == 'www.notfound.com':
+        return {'status_code': 404,
+                'content': 'Page not found'}
+
+    if url.path == '/console.html' and url.netloc == 'www.notfound.com':
+        return {'status_code': 403,
+                'content': None}
 
 
 class TestUtils(unittest.TestCase):
@@ -21,3 +53,34 @@ class TestUtils(unittest.TestCase):
             html = utils.get_html(self.url)
             self.assertEquals(html.content, b'Feeling lucky?')
             self.assertEquals(html.status_code, 200)
+
+    def test_get_console(self):
+        with HTTMock(get_console_log):
+            console, date, url = utils.get_console('https://www.found.com')
+            self.assertEquals(url, urljoin('https://www.found.com',
+                                           'console.html.gz'))
+            self.assertIsInstance(date, datetime.datetime)
+            self.assertIsNotNone(console)
+
+        with HTTMock(get_console_log):
+            console, date, url = utils.get_console(self.url)
+            self.assertEquals(console, None)
+            self.assertEquals(date, None)
+            self.assertEquals(url, None)
+
+        with HTTMock(get_console_log):
+            console, date, url = utils.get_console('https://www.notfound.com')
+            self.assertEquals(console, None)
+            self.assertEquals(date, None)
+            self.assertEquals(url, None)
+
+    def test_get_tests_results(self):
+        console_error_failure = _get_console_content()
+        failed, ok, errors = utils.get_tests_results(console_error_failure)
+        self.assertTrue(len(failed))
+        self.assertTrue(len(errors))
+        self.assertTrue(len(ok))
+        self.assertIn(('tempest.tests.lib.test_rest_client.'
+                       'TestResponseBodyList.test_str'), failed)
+        self.assertIn(('tempest.tests.test_wrappers.TestWrappers.'
+                       'test_pretty_tox_serial'), errors)
