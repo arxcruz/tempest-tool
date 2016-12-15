3ef832db2464f018c16097f8d906466b1e7b7583
Arx Cruz <arxcruz@redhat.com>
2016-12-15 20:30:31 +0100

    Add the logic to run the jobs

--- tempestmail/tempest_mail.py
+++ tempestmail/tempest_mail.py
@@ -1,7 +1,10 @@
 import logging
 import threading
+from apscheduler.schedulers.background import BackgroundScheduler
+import apscheduler.triggers.cron
 
 import config as tempest_config
+import mail
 import utils
 
 
@@ -13,16 +16,71 @@ class TempestMail(threading.Thread):
         self.config_file = config_file
         self.downstream = downstream
         self.upstream = upstream
-        self.config = self.loadConfig()
+        self.config = None
+        self._stopped = False
+        self._wake_condition = threading.Condition()
+        self.apsched = None
+
+    def stop(self):
+        self._stopped = True
+        self._wake_condition.acquire()
+        self._wake_condition.notify()
+        self._wake_condition.release()
+        self.log.debug('Finished stopping')
+
+    def startup(self):
+        self.updateConfig()
+
+    def reconfigureCron(self, config):
+        cron_map = {
+            'check': self.checkJobs,
+        }
+        if not self.apsched:
+            self.apsched = BackgroundScheduler()
+            self.apsched.start()
+            self.log.debug('Scheduler started')
+
+        for c in config.crons.values():
+            if ((not self.config) or
+               c.timespec != self.config.crons[c.name].timespec):
+                if self.config and self.config.crons[c.name].job:
+                    self.config.crons[c.name].job.remove()
+                parts = c.timespec.split()
+                if len(parts) > 5:
+                    second = parts[5]
+                else:
+                    second = None
+                minute, hour, dom, month, dow = parts[:5]
+                trigger = apscheduler.triggers.cron.CronTrigger(
+                    month=month, day=dom, day_of_week=dow,
+                    hour=hour, minute=minute, second=second)
+                c.job = self.apsched.add_job(
+                    cron_map[c.name], trigger=trigger)
+                self.log.debug('Cron job ')
+            else:
+                c.job = self.config.crons[c.name].job
 
     def run(self):
-        self._run()
+        try:
+            self.startup()
+        except Exception:
+            self.log.exception("Exception in startup:")
 
-    def _run(self):
-        data = []
+        while not self._stopped:
+            try:
+                self.log.debug('Calling updateConfig from while loop')
+                self.updateConfig()
+            except Exception:
+                self.log.exception("Exception in main loop:")
+            self._wake_condition.acquire()
+            self._wake_condition.wait(10)
+            self._wake_condition.release()
+
+    def checkJobs(self):
         if(self.upstream):
             for job in self.config.jobs:
-                index = job.get_index()
+                data = []
+                index = self.config.jobs[job].get_index()
                 for run in index:
                     console, date, link = utils.get_console(run)
                     if not console or not date:
@@ -49,22 +107,20 @@ class TempestMail(threading.Thread):
                         d['run'] = False
                     data.append(d)
 
-        data = sorted(data, key=lambda x: x['date'])
-        last = data[-1]
-        if last.get('new') or not last.get('run'):
-            # some_stats = stats(d)
-            # send_report_with_failures(last)
-            pass
-        elif last.get('failed') and not last.get('new'):
-            # send_successful_report(last=last)
-            pass
-        elif (last.get('ok') and not last.get('failed')
-              and not last.get('errors')):
-            # send_successful_report(last=last, nofail=True)
-            pass
-        else:
-            # send_unexpected_failure(last)
-            pass
+                data = sorted(data, key=lambda x: x['date'])
+                last = data[-1]
+                send_mail = mail.Mail(self.config)
+                send_mail.send_mail(self.config.jobs[job], last)
+
+    def updateConfig(self):
+        self.log.debug('Updating configuration from file')
+        config = self.loadConfig()
+        self.reconfigureCron(config)
+        self.setConfig(config)
+
+    def setConfig(self, config):
+        self.config = config
+        self.log.debug('Config changed')
 
     def loadConfig(self):
         self.log.debug("Loading configuration")
