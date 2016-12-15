ff0354005449012cbf36c762eafdab881ed1eddb
Arx Cruz <arxcruz@redhat.com>
2016-12-15 14:28:14 +0100

    Adding tempestmail command line script
    This code is to call the tempestmail without a daemon, it will run once
    and exit

---
+++ tempestmail/cmd/tempestmailcmd.py
@@ -0,0 +1,50 @@
+import argparse
+import sys
+
+import tempestmail.cmd
+
+from tempestmail.tempest_mail import TempestMail
+
+
+class TempestMailCmd(tempestmail.cmd.TempestMailApp):
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description='tempest-mail')
+        parser.add_argument('-c', dest='config',
+                            default='/etc/tempest-mail/tempest-mail.yaml',
+                            help='Path to config file')
+        parser.add_argument('-d', dest='nodaemon', action='store_true',
+                            help='Do not run as a daemon')
+        parser.add_argument('-l', dest='logconfig',
+                            help='Path to log config file')
+        parser.add_argument('-p', dest='pidfile',
+                            help='Path to pid file',
+                            default='/var/run/tempest-mail/tempest-mail.pid')
+        parser.add_argument('--version', dest='version',
+                            help='Show version')
+        parser.add_argument('--downstream', dest='downstream',
+                            action='store_true',
+                            help='Logs are downstream')
+        parser.add_argument('--upstream', dest='upstream',
+                            action='store_true',
+                            help='Logs are upstream',
+                            default=True)
+        self.args = parser.parse_args()
+
+    def main(self):
+        self.setup_logging()
+        self.mailcmd = TempestMail(self.args.config,
+                                   self.args.upstream,
+                                   self.args.downstream)
+        config = self.mailcmd.loadConfig()
+        self.mailcmd.setConfig(config)
+        self.mailcmd.checkJobs()
+
+
+def main():
+    tmc = TempestMailCmd()
+    tmc.parse_arguments()
+    tmc.setup_logging()
+    return tmc.main()
+
+if __name__ == '__main__':
+    sys.exit(main())
